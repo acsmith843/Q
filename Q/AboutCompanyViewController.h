baffb727d2698f5a50581e3899710be5c6597536
Adam C. Smith <acsmith843@gmail.com>
2014-11-30 10:31:55 -0500

    add new assets and introduce aboutcompanyviewcontroller

---
+++ Q/AboutCompanyViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutCompanyViewController.h
+//  Q
+//
+//  Created by Adam C. Smith on 11/30/14.
+//  Copyright (c) 2014 Adam C. Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutCompanyViewController : UIViewController
+
+@end
---
+++ Q/AboutCompanyViewController.m
@@ -0,0 +1,37 @@
+//
+//  AboutCompanyViewController.m
+//  Q
+//
+//  Created by Adam C. Smith on 11/30/14.
+//  Copyright (c) 2014 Adam C. Smith. All rights reserved.
+//
+
+#import "AboutCompanyViewController.h"
+
+@interface AboutCompanyViewController ()
+
+@end
+
+@implementation AboutCompanyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
